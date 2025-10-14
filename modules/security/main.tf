3cf00bb4c8f93a6fedd971c9130400f23020d18e
dangdat182 <danglequocdat182@github.com>
2025-10-14 09:38:21 +0700

    update iam

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,10 +1,17 @@
+# Bật Cloud Resource Manager API để thao tác IAM trên Project
+resource "google_project_service" "enable_crm" {
+  project = var.project_id
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+# (Nếu chưa có) đã bật IAM API
 resource "google_project_service" "enable_iam" {
   project = var.project_id
   service = "iam.googleapis.com"
 }
 
 resource "google_service_account" "sa" {
-  depends_on   = [google_project_service.enable_iam]
+  depends_on   = [google_project_service.enable_iam, google_project_service.enable_crm]
   account_id   = var.sa_id
   display_name = "Service Account for workloads"
 }
@@ -14,4 +21,6 @@ resource "google_project_iam_member" "bind" {
   project  = var.project_id
   role     = each.value
   member   = "serviceAccount:${google_service_account.sa.email}"
+
+  depends_on = [google_project_service.enable_crm]
 }
