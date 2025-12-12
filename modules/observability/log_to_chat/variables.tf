f198481d8a66192525cfed142b16ef9334c4efd7
dangdat182 <danglequocdat182@github.com>
2025-12-12 09:36:00 +0700

    fix10

--- modules/observability/log_to_chat/variables.tf
+++ modules/observability/log_to_chat/variables.tf
@@ -2,12 +2,12 @@ variable "project_id" {
   type = string
 }
 
-variable "log_filter" {
-  type        = string
-  description = "Logging filter for audit events to send to Chat"
-}
+# variable "log_filter" {
+#   type        = string
+#   description = "Logging filter for audit events to send to Chat"
+# }
 
-variable "chat_webhook_url" {
-  type        = string
-  description = "Google Chat incoming webhook URL"
-}
+# variable "chat_webhook_url" {
+#   type        = string
+#   description = "Google Chat incoming webhook URL"
+# }
